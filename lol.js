6661f5464d5f79656f5e6cfe201533a0e86ade99
djfariel <fariel@unibearstudio.com>
2020-09-11 18:33:04 -0400

    Update lol.js

--- lol.js
+++ lol.js
@@ -1,4 +1,4 @@
-var css = '.chat-message-list {background-image: url("https://cdn.discordapp.com/attachments/668546440020361228/754088080880959608/asdf.png");background-repeat: no-repeat, no-repeat;background-position: left top;}',
+var css = '.chat-message-list {background-image: url("https://cdn.discordapp.com/attachments/668546440020361228/754088080880959608/asdf.png");background-repeat: no-repeat, no-repeat;background-position: left top;background-size: 200px 200px;}',
     head = document.head || document.getElementsByTagName('head')[0],
     style = document.createElement('style');
 
