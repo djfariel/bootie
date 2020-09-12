fd784d338894ecf2d86f1912b92700de709a5af3
djfariel <fariel@unibearstudio.com>
2020-09-11 22:30:55 -0400

    Create seyes.js

---
+++ seyes.js
@@ -0,0 +1,13 @@
+var css = '.chat-message-list {background-image: url("https://cdn.discordapp.com/attachments/631040827124219904/754166036269498418/fdsa.png");background-repeat: no-repeat, no-repeat;background-position: right top;background-size: 175px 175px;}',
+    head = document.head || document.getElementsByTagName('head')[0],
+    style = document.createElement('style');
+
+head.appendChild(style);
+
+style.type = 'text/css';
+if (style.styleSheet){
+  // This is required for IE8 and below.
+  style.styleSheet.cssText = css;
+} else {
+  style.appendChild(document.createTextNode(css));
+}
